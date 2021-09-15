1c19fd0b6899f9790a5a2a4ca34e01fd3e02dc09
Manik <maniksharma589@gmail.com>
2021-09-15 13:58:32 +0530

    changes in top nav bar

--- layouts/partials/TopNav.vue
+++ layouts/partials/TopNav.vue
@@ -16,14 +16,19 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
             <b-navbar-nav>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/auth/login">Login</nuxt-link>
-                </li>
-            </b-navbar-nav>
-            <b-navbar-nav>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="/auth/register">Register</nuxt-link>
-                </li>
+                <template v-if="authenticated">
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/auth/login">Logout</nuxt-link>
+                     </li>
+                </template>
+                <template v-else>
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/auth/login">Login</nuxt-link>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link class="nav-link" to="/auth/register">Register</nuxt-link>
+                    </li>
+                </template>
             </b-navbar-nav>
             <b-nav-item-dropdown right>
                 <!-- Using 'button-content' slot -->
